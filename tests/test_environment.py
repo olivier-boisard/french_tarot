0078da20989e646c96e18c19671313fcb45348fb
Olivier Boisard <olivier.boisard@gmail.com>
2019-06-18 05:33:54 +0000

    Got rid of gym and implemented dealing to players.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,5 +1,26 @@
-from src.environment import FrenchTarotEnvironment
+import pytest
+
+from environment import Card, FrenchTarotEnvironment
 
 
-def test_create_environment():
-    FrenchTarotEnvironment()
+@pytest.fixture(scope="module")
+def environment():
+    yield FrenchTarotEnvironment()
+
+
+def test_n_cards():
+    assert len(list(Card))
+
+
+def test_reset_environment(environment):
+    observation = environment.reset()
+    assert len(observation["hand_per_player"]) == 4
+    for hands in observation["hand_per_player"]:
+        assert len(hands) == 18
+    assert len(observation["dog"]) == 6
+    assert "card_played_in_turn" in observation
+    assert "card_played_per_player_in_episode" in observation
+    assert "who_took" in observation
+    assert "is_dog_revealed" in observation
+    assert "bid" in observation
+    assert "announcements" in observation
