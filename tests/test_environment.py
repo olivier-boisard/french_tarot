96e29c0515a12481a44eb5361847381882f8972c
Olivier Boisard <olivier.boisard@gmail.com>
2019-06-19 04:37:27 +0000

    Adjusted unit tests.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -18,9 +18,10 @@ def test_reset_environment(environment):
     for hands in observation["hand_per_player"]:
         assert len(hands) == 18
     assert len(observation["dog"]) == 6
-    assert "card_played_in_turn" in observation
-    assert "card_played_per_player_in_episode" in observation
-    assert "who_took" in observation
-    assert "is_dog_revealed" in observation
-    assert "bid" in observation
-    assert "announcements" in observation
+    assert len(observation["card_played_in_turn"]) == 0
+    assert len(observation["card_played_per_player_in_episode"]) == 0
+    assert observation["who_took"] is None
+    assert not observation["is_dog_revealed"]
+    assert observation["bid"] is None
+    assert len(observation["announcements"]) == 0
+    assert not observation["is_game_started"]
