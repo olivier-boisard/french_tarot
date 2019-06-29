b121a5ac3cdf0d83bd168b6b2bdbf34d085f7ead
Olivier Boisard <olivier.boisard@gmail.com>
2019-06-29 17:10:09 +0000

    Updated unit test.

--- tests/environment/test_announcements_phase.py
+++ tests/environment/test_announcements_phase.py
@@ -103,6 +103,11 @@ def test_announce_simple_poignee_excuse_accepted():
     environment = FrenchTarotEnvironment()
     environment.reset()
     environment._deal(list(Card))
+    environment._hand_per_player[3][15] = Card.EXCUSE
+    environment._hand_per_player[3][16] = Card.SPADES_1
+    environment._hand_per_player[0][0] = Card.TRUMP_15
+    environment._original_dog[5] = Card.TRUMP_14
+
     environment.step(Bid.PASS)
     environment.step(Bid.PASS)
     environment.step(Bid.PASS)
@@ -111,8 +116,13 @@ def test_announce_simple_poignee_excuse_accepted():
     environment.step([])
     environment.step([])
     environment.step([])
-    observation = environment.step([Poignee(environment._hand_per_player[-1][-10:])])[0]
+
+    card_list = [Card.TRUMP_1, Card.TRUMP_2, Card.TRUMP_3, Card.TRUMP_4, Card.TRUMP_5, Card.TRUMP_6, Card.TRUMP_7,
+                 Card.TRUMP_8, Card.TRUMP_9, Card.TRUMP_10, Card.TRUMP_11, Card.TRUMP_12, Card.TRUMP_13, Card.EXCUSE,
+                 Card.TRUMP_16]
+    observation = environment.step([Poignee(card_list)])[0]
     assert isinstance(observation["announcements"][3], Poignee)
+    assert observation["announcements"][3]._revealed_cards == card_list
 
 
 def test_announce_simple_poignee_no_trump():
@@ -144,9 +154,9 @@ def test_announce_simple_poignee_no_such_cards_in_hand():
     environment.step([])
     environment.step([])
     with pytest.raises(ValueError):
-        environment.step([Poignee(
-            [Card.TRUMP_1, Card.TRUMP_2, Card.TRUMP_3, Card.TRUMP_4, Card.TRUMP_5, Card.TRUMP_6, Card.TRUMP_7,
-             Card.TRUMP_8, Card.TRUMP_9, Card.TRUMP_10])])
+        card_list = [Card.TRUMP_1, Card.TRUMP_2, Card.TRUMP_3, Card.TRUMP_4, Card.TRUMP_5, Card.TRUMP_6, Card.TRUMP_7,
+                     Card.TRUMP_8, Card.TRUMP_9, Card.TRUMP_21]
+        environment.step([Poignee(card_list)])
 
 
 def test_announce_poignee_invalid():
