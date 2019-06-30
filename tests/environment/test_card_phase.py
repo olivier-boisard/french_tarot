d33bc5ff1bc56d8c7dbc80620c4af4813df04394
Olivier Boisard <olivier.boisard@gmail.com>
2019-06-30 15:26:30 +0000

    Almost implemented unit tests for card phase.

--- tests/environment/test_card_phase.py
+++ tests/environment/test_card_phase.py
@@ -3,7 +3,7 @@ import pytest
 from environment import FrenchTarotEnvironment, Bid, Card, get_card_set_point
 
 
-def setup_envionment():
+def setup_environment():
     environment = FrenchTarotEnvironment()
     environment.reset()
     environment.step(Bid.GARDE_SANS)
@@ -18,7 +18,7 @@ def setup_envionment():
 
 
 def test_start_valid_card():
-    environment = setup_envionment()
+    environment = setup_environment()
     played_card = environment._hand_per_player[0][0]
     observation, reward, done, _ = environment.step(played_card)
     assert observation["played_cards"] == [played_card]
@@ -27,14 +27,14 @@ def test_start_valid_card():
 
 
 def test_start_invalid_card():
-    environment = setup_envionment()
+    environment = setup_environment()
     played_card = environment._hand_per_player[1][0]
     with pytest.raises(ValueError):
         environment.step(played_card)
 
 
 def test_play_complete_round_valid_last_player_team_wins():
-    environment = setup_envionment()
+    environment = setup_environment()
     environment.step(Card.HEART_1)
     environment.step(Card.HEART_KING)
     environment.step(Card.HEART_4)
@@ -50,7 +50,7 @@ def test_play_complete_round_valid_last_player_team_wins():
 
 
 def test_play_complete_round_valid_last_player_team_loses():
-    environment = setup_envionment()
+    environment = setup_environment()
     environment._current_player = 1
     environment.step(Card.HEART_KING)
     environment.step(Card.HEART_4)
@@ -67,7 +67,7 @@ def test_play_complete_round_valid_last_player_team_loses():
 
 
 def test_play_complete_round_valid_last_player_team_loses():
-    environment = setup_envionment()
+    environment = setup_environment()
     environment._current_player = 1
     environment.step(Card.HEART_KING)
     environment.step(Card.HEART_4)
@@ -84,7 +84,7 @@ def test_play_complete_round_valid_last_player_team_loses():
 
 
 def test_play_excuse_in_round():
-    environment = setup_envionment()
+    environment = setup_environment()
     environment._current_player = 2
     environment.step(Card.HEART_4)
     environment.step(Card.HEART_2)
@@ -101,7 +101,7 @@ def test_play_excuse_in_round():
 
 
 def test_play_excuse_first():
-    environment = setup_envionment()
+    environment = setup_environment()
     environment.step(Card.EXCUSE)
     environment.step(Card.HEART_KING)
     environment.step(Card.HEART_4)
@@ -116,37 +116,44 @@ def test_play_excuse_first():
     assert not done
 
 
-def test_play_trump_valid():
-    raise NotImplementedError()
-
-
 def test_play_trump_invalid():
-    raise NotImplementedError()
-
-
-def test_play_trump_above_trump():
-    raise NotImplementedError()
+    environment = setup_environment()
+    environment._hand_per_player = [[Card.SPADES_1, Card.SPADES_2], [Card.SPADES_3, Card.TRUMP_1]]
+    environment.step(Card.SPADES_1)
+    with pytest.raises(ValueError):
+        environment.step(Card.TRUMP_1)
 
 
 def test_play_trump_below_trump_unallowed():
-    raise NotImplementedError()
-
-
-def test_play_trump_below_trump_allowed():
-    raise NotImplementedError()
-
-
-def test_pee_allowed():
-    raise NotImplementedError()
+    environment = setup_environment()
+    environment._hand_per_player = [[Card.TRUMP_10, Card.TRUMP_11], [Card.TRUMP_1, Card.TRUMP_12]]
+    environment.step(Card.TRUMP_10)
+    with pytest.raises(ValueError):
+        environment.step(Card.TRUMP_1)
 
 
 def test_pee_not_allowed():
-    raise NotImplementedError()
+    environment = setup_environment()
+    environment._hand_per_player = [[Card.SPADES_1, Card.SPADES_2], [Card.HEART_4, Card.SPADES_3]]
+    environment.step(Card.SPADES_1)
+    with pytest.raises(ValueError):
+        environment.step(Card.HEART_4)
 
 
 def test_play_card_not_in_hand():
-    raise NotImplementedError()
+    environment = setup_environment()
+    environment._hand_per_player = [[Card.SPADES_1]]
+    with pytest.raises(ValueError):
+        environment.step(Card.HEART_4)
 
 
 def test_play_last_cards():
-    raise NotImplementedError()  # TODO make sure score is a rounded number
+    environment = setup_environment()
+    environment._hand_per_player = [[Card.SPADES_1], [Card.SPADES_2], [Card.HEART_4], [Card.SPADES_3]]
+    environment.step(Card.SPADES_1)
+    environment.step(Card.SPADES_2)
+    environment.step(Card.HEART_4)
+    observation, reward, done, _ = environment.step(Card.SPADES_3)
+    # TODO observation
+    # TODO reward
+    assert done
