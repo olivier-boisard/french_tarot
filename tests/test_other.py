d5e024dae8b954ddd81892335fc1b7c1e37fa92a
Olivier Boisard <olivier.boisard@gmail.com>
2019-07-31 05:16:09 +0000

    Added missing attribute when testing for specific bug fixes.

--- tests/test_other.py
+++ tests/test_other.py
@@ -10,10 +10,12 @@ def load_test_data(file_name):
     return obj
 
 
-def run_test_on_data(stuff):
+def run_test_on_data(stuff, mocker):
     obj = load_test_data(stuff)
+    if not hasattr(obj["environment"], '_original_player_ids'):
+        obj['environment']._original_player_ids = []
     observation, _, done, _ = obj["environment"].step(obj["agent"].get_action(obj["observation"]))
 
 
-def test_stress_test_iteration_433_wrong_assignment_type():
-    run_test_on_data("stress_test_433.dill")
+def test_stress_test_iteration_433_wrong_assignment_type(mocker):
+    run_test_on_data("stress_test_433.dill", mocker)
