639afd4a2b0f6f76de455f0a314c4982e721ec75
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-02 05:02:16 +0000

    Parallelized played games.

--- src/play_multiple_games.py
+++ src/play_multiple_games.py
@@ -6,44 +6,47 @@ import dill
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
+from joblib import Parallel, delayed
 
 from agent import RandomPlayer
 from environment import FrenchTarotEnvironment
 
 
 def _main():
-    environment = FrenchTarotEnvironment()
-    random_agent = RandomPlayer()
-    scores = []
-    for i in tqdm.tqdm(range(100)):
-        observation = environment.reset()
-        done = False
-        cnt = 0
-        while not done:
-            environment_copy = copy.deepcopy(environment)
-            random_agent_copy = copy.deepcopy(random_agent)
-            try:
-                observation, reward, done, _ = environment.step(random_agent.get_action(observation))
-            except ValueError as e:
-                obj = {"agent": random_agent_copy, "environment": environment_copy, "observation": observation,
-                       "done": done}
-                timestamp = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-").replace(".", "-")
-                output_file_path = os.path.join(os.path.dirname(__file__), "stress_test_{}.dill".format(timestamp))
-                print("Dumping file into " + output_file_path)
-                with open(output_file_path, "wb") as f:
-                    dill.dump(obj, f)
-                raise e
-            cnt += 1
-            if cnt >= 1000:
-                raise RuntimeError("Infinite loop")
-        offset = i % environment.n_players  # first player changes at each turn
-        game_scores = np.roll(np.array(reward)[observation["original_player_ids"]], offset)
-        if np.sum(game_scores) != 0:
-            RuntimeError("Scores do not sum up to 0")
-        scores.append(game_scores)
+    scores = Parallel(n_jobs=-1, verbose=10)(delayed(_run_game)(i) for i in tqdm.tqdm(range(100)))
 
     _print_final_scores(scores)
     _plot_scores(scores)
+
+
+def _run_game(iteration):
+    environment = FrenchTarotEnvironment(seed=iteration)
+    random_agent = RandomPlayer(seed=iteration)
+    observation = environment.reset()
+    done = False
+    cnt = 0
+    while not done:
+        environment_copy = copy.deepcopy(environment)
+        random_agent_copy = copy.deepcopy(random_agent)
+        try:
+            observation, reward, done, _ = environment.step(random_agent.get_action(observation))
+        except ValueError as e:
+            obj = {"agent": random_agent_copy, "environment": environment_copy, "observation": observation,
+                   "done": done}
+            timestamp = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-").replace(".", "-")
+            output_file_path = os.path.join(os.path.dirname(__file__), "stress_test_{}.dill".format(timestamp))
+            print("Dumping file into " + output_file_path)
+            with open(output_file_path, "wb") as f:
+                dill.dump(obj, f)
+            raise e
+        cnt += 1
+        if cnt >= 1000:
+            raise RuntimeError("Infinite loop")
+    offset = iteration % environment.n_players  # first player changes at each turn
+    game_scores = np.roll(np.array(reward)[observation["original_player_ids"]], offset)
+    if np.sum(game_scores) != 0:
+        RuntimeError("Scores do not sum up to 0")
+    return game_scores
 
 
 def _print_final_scores(scores):
