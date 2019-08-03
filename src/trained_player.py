ae074f947c3486daea78e13b4a21d00927b01fbe
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-03 15:05:56 +0000

    Added memory class.

--- src/trained_player.py
+++ src/trained_player.py
@@ -1,3 +1,6 @@
+import random
+from collections import namedtuple
+
 from torch import nn, tensor, argmax
 
 from environment import Card, Bid, GamePhase
@@ -5,6 +8,34 @@ from environment import Card, Bid, GamePhase
 
 def bid_phase_observation_encoder(observation):
     return tensor([card in observation["hand"] for card in list(Card)]).float()
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    """
+    Got from https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+    """
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
 
 class BidPhaseAgent:
