d6683772fe696f2a93ecf849323901830b6f7ab6
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-06 17:11:17 +0000

    Added reminder to mock batch size as urgent next task.

--- tests/test_play_trained_agent.py
+++ tests/test_play_trained_agent.py
@@ -5,4 +5,5 @@ def test_play_trained_agent(mocker):
     mocker.patch('play_trained_agent.dump_and_display_results')
     mocker.patch('play_trained_agent.DEVICE', "cpu")
     mocker.patch('play_trained_agent.N_ITERATIONS', 10)
+    assert False  # TODO mock batch size
     play_trained_agent._main()
