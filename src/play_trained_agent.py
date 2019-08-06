4ff1701771d77f8ad18348223c1254210f068fb6
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-06 16:00:56 +0000

    Chose more proper scaling factor base on the loss function return values.

--- src/play_trained_agent.py
+++ src/play_trained_agent.py
@@ -36,7 +36,7 @@ def _main():
             observation = new_observation
         for observation_to_save, action_to_save, reward in zip(observations_to_save, actions_to_save, rewards):
             bid_phase_dqn_agent.memory.push(bid_phase_observation_encoder(observation_to_save).unsqueeze(0),
-                                            action_to_save.value, None, reward / 1000.)
+                                            action_to_save.value, None, reward / 100.)
         all_rewards.append(np.roll(rewards, i % environment.n_players))
         bid_phase_dqn_agent.optimize_model()
 
