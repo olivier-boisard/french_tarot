0caf70ecfb1b6ce6ad27957fd25bd16004db7b31
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-12 04:53:22 +0000

    Renamed file.

--- tests/test_play_trained_agent.py
+++ tests/test_play_trained_agent.py
@@ -1,7 +1,7 @@
 from torch import nn
 
 from environment import Bid, Card
-from play_games import play_trained_agent
+from play_games import play_trained_agent_bid
 
 
 def test_play_trained_agent(mocker):
@@ -10,8 +10,8 @@ def test_play_trained_agent(mocker):
 
     mocker.patch('play_games.play_trained_agent.BidPhaseAgent._create_dqn', mock_create_dqn)
     mocker.patch('play_games.play_trained_agent.BidPhaseAgent.output_dimension', 5)
-    play_trained_agent._set_all_seeds()
-    bid_phase_dqn_agent = play_trained_agent.BidPhaseAgent(
+    play_trained_agent_bid._set_all_seeds()
+    bid_phase_dqn_agent = play_trained_agent_bid.BidPhaseAgent(
         eps_start=0.5, eps_end=0.05, eps_decay=50, batch_size=4, replay_memory_size=8, device="cpu"
     )
-    play_trained_agent._run_training(bid_phase_dqn_agent, n_iterations=10)
+    play_trained_agent_bid._run_training(bid_phase_dqn_agent, n_iterations=10)
