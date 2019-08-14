13a99dbac7d57c0ef7f4466d968e4c58c7400c97
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-14 04:49:01 +0000

    Fixed unit test.

--- tests/agents/test_common.py
+++ tests/agents/test_common.py
@@ -5,5 +5,5 @@ from agents.common import Agent
 
 def test_create_abstract_agent(mock):
     mock.patch("torch.optim.Adam")
-    with pytest.raises(RuntimeError):
+    with pytest.raises(TypeError):
         Agent(mock.Mock())
