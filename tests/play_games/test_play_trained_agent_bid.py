915da598cb6f8502e4bb50f88fe3cc27422a5672
Olivier Boisard <olivier.boisard@gmail.com>
2019-08-20 04:29:22 +0000

    Imported class the right way

--- tests/play_games/test_play_trained_agent_bid.py
+++ tests/play_games/test_play_trained_agent_bid.py
@@ -1,5 +1,7 @@
 from torch import nn
 
+from agents.trained_player import TrainedPlayer
+from agents.trained_player_bid import BidPhaseAgent
 from environment import Card
 from play_games import play_trained_agent_bid
 
@@ -11,7 +13,8 @@ def test_play_trained_agent_bid(mocker):
     mocker.patch('play_games.play_trained_agent_bid.BidPhaseAgent._create_dqn', mock_create_dqn)
     mocker.patch('play_games.play_trained_agent_bid.BidPhaseAgent.output_dimension', 5)
     play_trained_agent_bid._set_all_seeds()
-    bid_phase_dqn_agent = play_trained_agent_bid.BidPhaseAgent(
+    bid_phase_dqn_agent = BidPhaseAgent(
         eps_start=0.5, eps_end=0.05, eps_decay=50, batch_size=4, replay_memory_size=8, device="cpu"
     )
-    play_trained_agent_bid._run_training(bid_phase_dqn_agent, n_iterations=10)
+    agent = TrainedPlayer(bid_phase_agent=bid_phase_dqn_agent)
+    play_trained_agent_bid._run_training(agent, n_iterations=10)
