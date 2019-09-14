2777743207365cccac138f6c25ac16466097b40d
Olivier Boisard <olivier.boisard@gmail.com>
2019-09-14 10:22:43 +0000

    Fixed mocking.

--- tests/play_games/test_play_random_games.py
+++ tests/play_games/test_play_random_games.py
@@ -3,8 +3,8 @@ from french_tarot.play_games import play_random_games
 
 def test_play_random_games(mocker):
     mocker.patch("argparse.ArgumentParser.parse_args", lambda _: mocker.Mock(initial_seed=0))
-    mocker.patch('play_games.play_random_games.N_ITERATIONS', 10)
-    mocker.patch('play_games.play_random_games._print_final_scores')
-    mocker.patch('play_games.play_random_games._plot_scores')
+    mocker.patch('french_tarot.play_games.play_random_games.N_ITERATIONS', 10)
+    mocker.patch('french_tarot.play_games.play_random_games._print_final_scores')
+    mocker.patch('french_tarot.play_games.play_random_games._plot_scores')
     mocker.patch('torch.utils.tensorboard.SummaryWriter')
     play_random_games._main()
