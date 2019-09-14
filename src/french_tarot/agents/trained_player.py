aa0092dc5d7f63cc98adc25c396640d453fe9d53
Olivier Boisard <olivier.boisard@gmail.com>
2019-09-14 13:32:19 +0000

    Used polymorphism-.

--- src/french_tarot/agents/trained_player.py
+++ src/french_tarot/agents/trained_player.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 from french_tarot.agents.common import core, BaseCardNeuralNet, Agent
+from french_tarot.agents.meta import singledispatchmethod
 from french_tarot.agents.random_agent import RandomPlayer
 from french_tarot.agents.trained_player_bid import BidPhaseAgent
 from french_tarot.agents.trained_player_dog import DogPhaseAgent
@@ -13,10 +14,6 @@ from french_tarot.environment.observations import Observation, BidPhaseObservati
 
 
 class TrainedPlayer(Agent):
-
-    def optimize_model(self, tb_writer: SummaryWriter):
-        for model in self._agents.values():
-            model.optimize_model(tb_writer)
 
     def __init__(self, bid_phase_agent: torch.nn.Module = None, dog_phase_agent: torch.nn.Module = None):
         random_agent = RandomPlayer()
@@ -30,24 +27,35 @@ class TrainedPlayer(Agent):
             CardPhaseObservation.__name__: random_agent  # trained agent not implemented yet
         }
 
+    def optimize_model(self, tb_writer: SummaryWriter):
+        for model in self._agents.values():
+            model.optimize_model(tb_writer)
+
     def get_action(self, observation: Observation):
         return self._agents[observation.__class__.__name__].get_action(observation)
 
-    def push_to_agent_memory(self, observation: dict, action, reward: float):
-        if isinstance(observation, BidPhaseObservation):
-            self._agents[BidPhaseObservation.__name__].memory.push(core(observation.hand).unsqueeze(0),
-                                                                   action,
-                                                                   None, reward)
-        elif isinstance(observation, DogPhaseObservation):
-            selected_cards = torch.zeros(len(CARDS))
-            for permuted_action in itertools.permutations(action):
-                hand = list(observation.hand)
-                for card in permuted_action:
-                    xx = torch.cat((core(hand), selected_cards)).unsqueeze(0)
-                    action_id = DogPhaseAgent.CARDS_OK_IN_DOG.index(card)
-                    self._agents[DogPhaseObservation.__name__].memory.push(xx, action_id, None, reward)
+    @singledispatchmethod
+    def push_to_agent_memory(self, observation: Observation, action, reward: float):
+        raise ValueError("Unhandled observation type")
 
-                    selected_cards[action_id] = 1
-                    hand.remove(DogPhaseAgent.CARDS_OK_IN_DOG[action_id])
-        else:
-            raise NotImplemented()
+    @push_to_agent_memory.register
+    def _(self, observation: DogPhaseObservation, action, reward):
+        selected_cards = torch.zeros(len(CARDS))
+        for permuted_action in itertools.permutations(action):
+            hand = list(observation.hand)
+            for card in permuted_action:
+                xx = torch.cat((core(hand), selected_cards)).unsqueeze(0)
+                action_id = DogPhaseAgent.CARDS_OK_IN_DOG.index(card)
+                self._agents[DogPhaseObservation.__name__].memory.push(xx, action_id, None, reward)
+
+                selected_cards[action_id] = 1
+                hand.remove(DogPhaseAgent.CARDS_OK_IN_DOG[action_id])
+
+    @push_to_agent_memory.register
+    def _(self, observation: BidPhaseObservation, action, reward):
+        self._agents[BidPhaseObservation.__name__].memory.push(
+            core(observation.hand).unsqueeze(0),
+            action,
+            None,
+            reward
+        )
