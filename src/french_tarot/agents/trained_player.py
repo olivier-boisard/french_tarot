d6ad5b701da6097a118e331ed6cc8f47cf1679a1
OBoisard <OBoisard@agtinternational.com>
2019-09-17 16:07:40 +0200

    USed AgentWithTrainer class.

--- src/french_tarot/agents/trained_player.py
+++ src/french_tarot/agents/trained_player.py
@@ -1,6 +1,10 @@
+from abc import ABC
+from typing import Union
+
+from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 
-from french_tarot.agents.common import CoreCardNeuralNet, Agent
+from french_tarot.agents.common import CoreCardNeuralNet, Agent, Trainer
 from french_tarot.agents.random_agent import RandomPlayer
 from french_tarot.agents.trained_player_bid import BidPhaseAgent, BidPhaseAgentTrainer
 from french_tarot.agents.trained_player_dog import DogPhaseAgent, DogPhaseAgentTrainer
@@ -8,37 +12,56 @@ from french_tarot.environment.observations import Observation, BidPhaseObservati
     AnnouncementPhaseObservation, CardPhaseObservation
 
 
+class DummyTrainer:
+
+    def optimize_model(self, *args, **kwargs):
+        pass
+
+
+class AgentWithTrainer(ABC):
+    def __init__(self, agent: Agent, trainer: Union[Trainer, DummyTrainer]):
+        self.agent = agent
+        self.trainer = trainer
+
+
+class RandomAgentWithDummyTrainer(AgentWithTrainer):
+    def __init__(self, agent):
+        super().__init__(agent, DummyTrainer())
+
+
+class BidPhaseAgentWithTrainer(AgentWithTrainer):
+    def __init__(self, base_card_neural_net: nn.Module, summary_writer: SummaryWriter = None):
+        agent = BidPhaseAgent(base_card_neural_net)
+        trainer = BidPhaseAgentTrainer(agent.policy_net, summary_writer=summary_writer)
+        super().__init__(agent, trainer)
+
+
+class DogPhaseAgentWithTrainer(AgentWithTrainer):
+    def __init__(self, base_card_neural_net: nn.Module, summary_writer: SummaryWriter = None):
+        agent = DogPhaseAgent(base_card_neural_net)
+        trainer = DogPhaseAgentTrainer(agent.policy_net, summary_writer=summary_writer)
+        super().__init__(agent, trainer)
+
+
+# TODO finish
 class AllPhasePlayerTrainer(Agent):
 
     def __init__(self, summary_writer: SummaryWriter = None, **kwargs):
         super().__init__(**kwargs)
-        self._initialize_per_phase_agents()
-        self._initialize_trainers(summary_writer)
+        self._initialize_per_phase_agents(summary_writer)
 
-    def _initialize_per_phase_agents(self):
+    def _initialize_per_phase_agents(self, summary_writer):
         base_card_neural_net = CoreCardNeuralNet()
-        self._agents = {
-            BidPhaseObservation: BidPhaseAgent(base_card_neural_net),
-            DogPhaseObservation: DogPhaseAgent(base_card_neural_net),
-            AnnouncementPhaseObservation: RandomPlayer(),
-            CardPhaseObservation: RandomPlayer()
-        }
-
-    def _initialize_trainers(self, summary_writer):
-        self._trainers = {
-            BidPhaseObservation: BidPhaseAgentTrainer(
-                self._agents[BidPhaseObservation].policy_net,
-                summary_writer=summary_writer
-            ),
-            DogPhaseObservation: DogPhaseAgentTrainer(
-                self._agents[DogPhaseObservation].policy_net,
-                summary_writer=summary_writer
-            )
+        self._agents_with_trainers = {
+            BidPhaseObservation: BidPhaseAgentWithTrainer(base_card_neural_net, summary_writer=summary_writer),
+            DogPhaseObservation: DogPhaseAgentWithTrainer(base_card_neural_net, summary_writer=summary_writer),
+            AnnouncementPhaseObservation: RandomAgentWithDummyTrainer(RandomPlayer()),
+            CardPhaseObservation: RandomAgentWithDummyTrainer(RandomPlayer())
         }
 
     def optimize_model(self):
-        for model in self._trainers.values():
+        for model in self._agents_with_trainers.values():
             model.optimize_model()
 
     def get_action(self, observation: Observation):
-        return self._agents[observation.__class__].get_action(observation)
+        return self._agents_with_trainers[observation.__class__].agent.get_action(observation)
