3c025cb8811efa0e189a9d6b4c29345d444334e2
OBoisard <OBoisard@agtinternational.com>
2019-09-17 17:36:30 +0200

    Starting breaking down environment.

--- src/french_tarot/environment/environment.py
+++ src/french_tarot/environment/environment.py
@@ -50,6 +50,87 @@ def check_card_is_allowed(card: Card, played_cards: List[Card], player_hand: Lis
         check_trump_value_is_allowed(card, played_cards, player_hand)
 
 
+class BidPhaseEnvironment:
+
+    def __init__(self, hand_per_player: List[List[Card]], original_dog=List[Card]):
+        self._hand_per_player = hand_per_player
+        self._original_dog = original_dog
+        self._original_player_ids = None
+        self._bid_per_player = []
+        self._starting_player = None
+        self.taking_player_original_id = None
+        self.current_player = None
+        self._game_phase = None
+
+    @property
+    def n_players(self):
+        return len(self._hand_per_player)
+
+    # TODO create smaller functions
+    def bid(self, action: Bid) -> Tuple[float, bool, any]:
+        self._check_input(action)
+        self._bid_per_player.append(action)
+
+        if len(self._bid_per_player) == self.n_players:
+            everybody_passed = np.all(np.array(self._bid_per_player) == Bid.PASS)
+            # noinspection PyTypeChecker
+            taking_player = int(np.argmax(self._bid_per_player))
+            self._shift_taking_player_to_id_0(taking_player)
+            self._check_state_consistency()
+            if not self.dog_phase_should_be_skipped():
+                self._prepare_for_dog_phase()
+            else:
+                self._prepare_for_announcement_phase()
+        else:
+            everybody_passed = False
+            
+        info = None
+        reward = 0
+        return reward, everybody_passed, info
+
+    def dog_phase_should_be_skipped(self):
+        skip_dog_phase = np.max(self._bid_per_player) > Bid.GARDE
+        return skip_dog_phase
+
+    def _prepare_for_announcement_phase(self):
+        self._game_phase = GamePhase.ANNOUNCEMENTS
+        self.current_player = 0  # taker makes announcements first
+
+    def _prepare_for_dog_phase(self):
+        self._hand_per_player[0] = self._hand_per_player[0] + self._original_dog
+        self._game_phase = GamePhase.DOG
+        self.current_player = self._starting_player
+
+    def _shift_taking_player_to_id_0(self, taking_player):
+        original_player_ids = np.arange(taking_player, taking_player + self.n_players) % self.n_players
+        self._original_player_ids = list(original_player_ids)
+        self._bid_per_player = rotate_list(self._bid_per_player, -taking_player)
+        self._hand_per_player = rotate_list(self._hand_per_player, -taking_player)
+        self._starting_player = -taking_player % self.n_players
+        self.taking_player_original_id = taking_player
+
+    def _check_state_consistency(self):
+        assert np.argmax(self._bid_per_player) == 0
+
+    def _check_input(self, action):
+        if type(action) != Bid:
+            raise FrenchTarotException("Wrong type for 'action'")
+        if action != Bid.PASS and action < get_minimum_allowed_bid(self._bid_per_player):
+            raise FrenchTarotException("Action is not pass and is lower than highest bid")
+
+
+class DogPhaseEnvironment:
+    pass  # TODO
+
+
+class AnnouncementPhaseEnvironment:
+    pass  # TODO
+
+
+class CardPhaseEnvironment:
+    pass  # TODO
+
+
 # TODO create smaller classes
 class FrenchTarotEnvironment:
 
@@ -362,44 +443,6 @@ class FrenchTarotEnvironment:
         self._made_dog = dog
         done = False
         info = None
-        return reward, done, info
-
-    # TODO create smaller functions
-    def _bid(self, action: Bid) -> Tuple[float, bool, any]:
-        if type(action) != Bid:
-            raise FrenchTarotException("Wrong type for 'action'")
-
-        if action != Bid.PASS and action < get_minimum_allowed_bid(self._bid_per_player):
-            raise FrenchTarotException("Action is not pass and is lower than highest bid")
-
-        self._bid_per_player.append(action)
-        self.current_player = len(self._bid_per_player)
-        reward = 0
-        if len(self._bid_per_player) == self.n_players:
-            done = np.all(np.array(self._bid_per_player) == Bid.PASS)
-            # noinspection PyTypeChecker
-            taking_player = int(np.argmax(self._bid_per_player))
-            original_player_ids = np.arange(taking_player, taking_player + self.n_players) % self.n_players
-            self._original_player_ids = list(original_player_ids)
-            self._bid_per_player = rotate_list(self._bid_per_player, -taking_player)
-            assert np.argmax(self._bid_per_player) == 0
-            self._hand_per_player = rotate_list(self._hand_per_player, -taking_player)
-            self._starting_player = -taking_player % self.n_players
-            self.taking_player_original_id = taking_player
-            if np.max(self._bid_per_player) <= Bid.GARDE:
-                self._hand_per_player[0] = self._hand_per_player[0] + self._original_dog
-                self._game_phase = GamePhase.DOG
-                self.current_player = self._starting_player
-            else:
-                self._game_phase = GamePhase.ANNOUNCEMENTS
-                self.current_player = 0  # taker makes announcements first
-        else:
-            done = False
-        info = None
-
-        if done:
-            reward = [0] * self.n_players
-
         return reward, done, info
 
     def reset(self) -> Observation:
