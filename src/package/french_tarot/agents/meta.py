04571a3c77663951309355f2290c9eebed4f3eeb
Olivier Boisard <olivier.boisard@gmail.com>
2019-09-23 19:08:38 +0000

    Formatted code.

--- src/package/french_tarot/agents/meta.py
+++ src/package/french_tarot/agents/meta.py
@@ -5,38 +5,6 @@ from french_tarot.exceptions import FrenchTarotException
 _singledispatch_registry = {}
 
 
-def _get_first_arg_type(func_overload):
-    return list(func_overload.__annotations__.values())[0]
-
-
-def _determine_func_to_call(arg_type, registry):
-    func_to_call = _recursively_determine_func_to_call([arg_type], registry)
-    if func_to_call is None:
-        func_to_call = registry["default"]
-    return func_to_call
-
-
-def _add_type_to_registry_if_not_there(arg_type, func_to_call, registry):
-    if arg_type not in registry:
-        registry[arg_type] = func_to_call
-
-
-def _recursively_determine_func_to_call(arg_types: type, registry: dict):
-    if len(arg_types) == 0:
-        func_to_call = None
-    else:
-        new_arg_types = []
-        func_to_call = None
-        for arg_type in arg_types:
-            func_to_call = registry.get(arg_type)
-            if func_to_call is not None:
-                break
-            new_arg_types.extend(arg_type.__bases__)
-        if func_to_call is None:
-            func_to_call = _recursively_determine_func_to_call(new_arg_types, registry)
-    return func_to_call
-
-
 def singledispatchmethod(func):
     """Provides method overloading.
 
@@ -64,3 +32,36 @@ def singledispatchmethod(func):
     wrapper.register = register
     update_wrapper(wrapper, func)
     return wrapper
+
+
+def _get_first_arg_type(func_overload):
+    return list(func_overload.__annotations__.values())[0]
+
+
+def _determine_func_to_call(arg_type, registry):
+    func_to_call = _recursively_determine_func_to_call([arg_type], registry)
+    if func_to_call is None:
+        func_to_call = registry["default"]
+    return func_to_call
+
+
+def _add_type_to_registry_if_not_there(arg_type, func_to_call, registry):
+    if arg_type not in registry:
+        registry[arg_type] = func_to_call
+
+
+def _recursively_determine_func_to_call(arg_types: type, registry: dict):
+    if len(arg_types) == 0:
+        func_to_call = None
+    else:
+        func_to_call = None
+        new_arg_types = []
+        for arg_type in arg_types:
+            func_to_call = registry.get(arg_type)
+            if func_to_call is not None:
+                break
+            new_arg_types.extend(arg_type.__bases__)
+
+        if func_to_call is None:
+            func_to_call = _recursively_determine_func_to_call(new_arg_types, registry)
+    return func_to_call
