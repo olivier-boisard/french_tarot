aefaf4b272d88fbae6abd9e2cd16be607245946e
Olivier Boisard <olivier.boisard@gmail.com>
2019-10-15 04:54:37 +0000

    Fixed mock.

--- src/tests/play_games/test_play_trained_agent.py
+++ src/tests/play_games/test_play_trained_agent.py
@@ -8,7 +8,7 @@ def test_play_trained_agent_bid_dog(mocker):
     def mock_create_dog_phase_dqn(_):
         return nn.Sequential(nn.Linear(len(CARDS), 1), nn.Sigmoid())
 
-    mocker.patch('french_tarot.agents.trained_player_bid.BidPhaseAgent._create_dqn', mock_create_dog_phase_dqn)
+    mocker.patch('french_tarot.agents.trained_player_bid.BidPhaseAgent.create_dqn', mock_create_dog_phase_dqn)
     mocker.patch('french_tarot.agents.trained_player_bid.BidPhaseAgent.output_dimension', 5)
     mocker.patch('torch.utils.tensorboard.SummaryWriter')
     play_trained_agent.set_all_seeds()
