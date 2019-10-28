27ed38593b832ddda582e093f4432da9bfeb40df
Olivier Boisard <olivier.boisard@gmail.com>
2019-10-28 07:24:40 +0000

    Added unit test for bugfix.

--- src/tests/agents/test_random_agent.py
+++ src/tests/agents/test_random_agent.py
@@ -37,10 +37,19 @@ def test_play_game(random_agent, environment):
     # No assert needed here, the code just needs to run without raising exceptions
 
 
-def test_bugfix_announcement_phase():
+def test_bugfix_01():
     agent = RandomPlayer()
     hand = [Card.TRUMP_14, Card.TRUMP_8, Card.EXCUSE, Card.TRUMP_21, Card.HEART_8, Card.TRUMP_2, Card.CLOVER_KING,
             Card.SPADES_10, Card.DIAMOND_6, Card.SPADES_9, Card.TRUMP_15, Card.SPADES_QUEEN, Card.TRUMP_20,
             Card.SPADES_3, Card.TRUMP_7, Card.TRUMP_10, Card.TRUMP_6, Card.TRUMP_4]
+    action = agent.get_action(AnnouncementPhaseObservation(PlayerData(0, hand)))
+    assert Card.EXCUSE not in action[0].revealed_cards
+
+
+def test_bugfix_02():
+    hand = [Card.DIAMOND_9, Card.HEART_7, Card.EXCUSE, Card.SPADES_JACK, Card.HEART_KING, Card.TRUMP_4, Card.TRUMP_8,
+            Card.TRUMP_21, Card.CLOVER_KING, Card.TRUMP_1, Card.HEART_1, Card.TRUMP_5, Card.TRUMP_6, Card.TRUMP_20,
+            Card.TRUMP_19, Card.SPADES_7, Card.TRUMP_9, Card.TRUMP_13]
+    agent = RandomPlayer()
     action = agent.get_action(AnnouncementPhaseObservation(PlayerData(0, hand)))
     assert Card.EXCUSE not in action[0].revealed_cards
