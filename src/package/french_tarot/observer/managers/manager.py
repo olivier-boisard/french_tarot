b0cd41a78921342ca2829558f6577dbf41009757
Olivier Boisard <olivier.boisard@gmail.com>
2019-10-30 06:30:02 +0000

    Revert "Added warmup."
    This reverts commit b3f1ea8c

--- src/package/french_tarot/observer/managers/manager.py
+++ src/package/french_tarot/observer/managers/manager.py
@@ -1,5 +1,4 @@
 import itertools
-from threading import Lock
 from typing import Dict, List
 
 import dill
@@ -15,29 +14,22 @@ class Manager(AbstractManager):
     def __init__(self):
         super().__init__()
         self._event_subscriber_map: Dict[EventType, List[AbstractSubscriber]] = {}
-        self._lock = Lock()
 
     def add_subscriber(self, subscriber: AbstractSubscriber, event_type: 'EventType'):
-        self._lock.acquire()
         if event_type not in self._event_subscriber_map:
             self._event_subscriber_map[event_type] = []
         self._event_subscriber_map[event_type].append(subscriber)
-        self._lock.release()
 
     def publish(self, message: Message):
-        self._lock.acquire()
         if message.event_type in self._event_subscriber_map:
             for subscriber in self._event_subscriber_map[message.event_type]:
                 subscriber.push(message.data)
-        self._lock.release()
 
     def dump_history(self, output_path: str):
-        self._lock.acquire()
         subscribers = set(itertools.chain(*self._event_subscriber_map.values()))
         output = {subscriber.__class__.__name__: subscriber.input_history for subscriber in subscribers}
         with open(output_path, "wb") as f:
             dill.dump(output, f)
-        self._lock.release()
 
     @classmethod
     def load_history(cls, path):
