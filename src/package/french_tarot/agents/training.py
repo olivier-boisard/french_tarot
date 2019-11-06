4f0e08b2da3cb571e1edad74021b7a25dd74f2b3
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-06 07:32:19 +0100

    Removed obsolete ReplayMemory class.

--- src/package/french_tarot/agents/training.py
+++ src/package/french_tarot/agents/training.py
@@ -1,10 +1,7 @@
-import random
 from collections import namedtuple
-from typing import List
 
 import math
 import numpy as np
-import torch
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
@@ -20,28 +17,3 @@ class Policy:
         threshold = self._eps_end + (self._eps_start - self._eps_end) * math.exp(-1. * step / self._eps_decay)
         plays_at_random = self._random_state.rand(1, 1) > threshold
         return plays_at_random
-
-
-class ReplayMemory:
-    """
-    Got from https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
-    """
-
-    def __init__(self, capacity: int, random_seed: int = 0):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-        self._random_state = random.Random(random_seed)
-
-    def push_message(self, state: torch.Tensor, action: int, next_state: torch.Tensor, reward: float):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(state, action, next_state, reward)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size: int) -> List[Transition]:
-        return self._random_state.sample(self.memory, batch_size)
-
-    def __len__(self) -> int:
-        return len(self.memory)
