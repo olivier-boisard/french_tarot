33ed52d302626642d7461143274805a3736795a8
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-07 07:33:37 +0100

    Unskip features.

--- src/tests/agents/test_trained_player_card.py
+++ src/tests/agents/test_trained_player_card.py
@@ -1,41 +1,30 @@
-import pytest
-
-
-@pytest.mark.skip
 def test_cuts_feature():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_pees_feature():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_remaining_figures_per_colors():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_excuse_context():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_n_big_trumps_still_in_game():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_petit_still_in_game():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_taker_feature():
     raise NotImplementedError()
 
 
-@pytest.mark.skip
 def test_n_trumps_still_in_game():
     raise NotImplementedError()
