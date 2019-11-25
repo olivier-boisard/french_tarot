767e4a94466d3bc514052de48195e027cfbbb6bd
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-25 07:35:19 +0100

    Use mocker fixture instead of mock.

--- src/tests/unit/agents/test_common.py
+++ src/tests/unit/agents/test_common.py
@@ -3,7 +3,7 @@ import pytest
 from french_tarot.agents.neural_net import BaseNeuralNetAgent
 
 
-def test_create_abstract_agent(mock):
-    mock.patch("torch.optim.Adam")
+def test_create_abstract_agent(mocker):
+    mocker.patch("torch.optim.Adam")
     with pytest.raises(TypeError):
-        BaseNeuralNetAgent(mock.Mock())
+        BaseNeuralNetAgent(mocker.Mock())
