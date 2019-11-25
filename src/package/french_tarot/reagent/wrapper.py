290e4df970e9fdf1d470c182240006ded6f1d770
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-25 08:14:00 +0100

    Converted to ReAgent wrapper.

---
+++ src/package/french_tarot/reagent/wrapper.py
@@ -0,0 +1,2 @@
+def convert_to_timeline_format():
+    pass
--- src/package/french_tarot/run.py
+++
@@ -1,6 +0,0 @@
-class Run:
-    def __init__(self, n_episodes: int, ):
-        self._n_episodes = n_episodes
-
-    def convert_to_timeline_format(self):
-        pass
