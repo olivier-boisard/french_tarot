339afd8095101597df6db56daca1a3fbee978d58
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-25 08:15:09 +0100

    Made unit test fail.

--- src/tests/integration/reagent/test_wrapper.py
+++ src/tests/integration/reagent/test_wrapper.py
@@ -1,9 +1,12 @@
 import os
 
 from french_tarot.play import create_batch
+from french_tarot.reagent.wrapper import convert_to_timeline_format
 
 
 def test_convert_to_timeline_format(request):
     batch_filepath = os.path.join(".tmp", "dummy.json")
     request.addfinalizer(lambda: os.remove(batch_filepath))
     create_batch(n_episodes=2, output_file_path=batch_filepath)
+
+    convert_to_timeline_format(batch_filepath)
