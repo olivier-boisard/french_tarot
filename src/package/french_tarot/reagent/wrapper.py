1e84159ce31eccd968fe98d3249b83e126923628
Olivier Boisard <olivier.boisard@gmail.com>
2019-11-26 07:31:55 +0100

    Implemented generate_timeline.

--- src/package/french_tarot/reagent/wrapper.py
+++ src/package/french_tarot/reagent/wrapper.py
@@ -1,2 +1,57 @@
-def convert_to_timeline_format():
-    pass
+import json
+import os
+from dataclasses import dataclass
+from typing import List, Dict
+
+from french_tarot.environment import french_tarot
+from french_tarot.environment.core import CARDS
+from french_tarot.reagent.data import ReAgentDataRow
+
+
+def convert_to_timeline_format(batch: List[ReAgentDataRow], output_folder: str):
+    docker_workdir = os.path.join(os.path.dirname(french_tarot.__file__), os.pardir, os.pardir, os.pardir, os.pardir)
+    docker_workdir = os.path.abspath(docker_workdir)
+
+    docker_command = [
+        "docker run",
+        "--rm",
+        "--runtime-nvidia",
+        "-v {}:{}".format(docker_workdir, docker_workdir),
+        "-w {}".format(os.path.join(docker_workdir, "ReAgent")),
+        "-p 0.0.0.0:6006:6006",
+        "horizon:dev"
+    ]
+
+    docker_run_command = [
+        os.path.join("/", "usr", "local", "spark", "bin", "spark-submit"),
+        "--class com.facebook.spark.rl.Preprocessor preprocessing/target/rl-preprocessing-1.1.jar",
+        "{}".format(json.dumps(generate_timeline(batch)))
+    ]
+
+
+def generate_timeline(batch: List[ReAgentDataRow]) -> 'Timeline':
+    datasets_ids = [row.ds for row in batch]
+
+    # We use a dict instead of a dataclass here because attribute names must be lowerCamelCase and this would break
+    # PEP8
+    timeline = {
+        "startDs": min(datasets_ids),
+        "endDs": max(datasets_ids),
+        "addTerminalStateRow": True,  # final row in each MDP is corresponds to the terminal state
+        "actionDiscrete": True,
+        "inputTableName": "french_tarot_discrete",
+        "outputTableName": "french_tarot_discrete_training",
+        "evalTableName": "french_tarot_discrete_eval",
+        "numOutputShards": 1,  # some Spark stuff that should be kept to 1 in case of one machine only process
+    }
+    query = {
+        "tableSample": 100,
+        "actions": [str(i) for i in range(len(CARDS))]
+    }
+    return Timeline(timeline=timeline, query=query)
+
+
+@dataclass
+class Timeline:
+    timeline: Dict
+    query: Dict
--- src/tests/integration/reagent/test_wrapper.py
+++ src/tests/integration/reagent/test_wrapper.py
@@ -1,12 +1,43 @@
+import ast
 import os
+import shutil
 
-from french_tarot.play import create_batch
-from french_tarot.reagent.wrapper import convert_to_timeline_format
+import pytest
+
+from french_tarot.play import play_episodes
+from french_tarot.reagent.wrapper import convert_to_timeline_format, generate_timeline
 
 
-def test_convert_to_timeline_format(request):
-    batch_filepath = os.path.join(".tmp", "dummy.json")
-    request.addfinalizer(lambda: os.remove(batch_filepath))
-    create_batch(n_episodes=2, output_file_path=batch_filepath)
+@pytest.fixture
+def batch():
+    return play_episodes(10)
 
-    convert_to_timeline_format(batch_filepath)
+
+def test_convert_to_timeline_format(request, batch):
+    output_folder = "dummy_training_data"
+    request.addfinalizer(lambda: shutil.rmtree(output_folder))
+
+    convert_to_timeline_format(batch, output_folder)
+    assert os.path.isfile(os.path.join(output_folder, "french_tarot_discrete_timeline.json"))
+    assert os.path.isfile(os.path.join(output_folder, "french_tarot_discrete_timeline_eval.json"))
+
+
+def test_generate_timeline(batch):
+    output = generate_timeline(batch)
+    timeline = output.timeline
+    query = output.query
+    assert isinstance(timeline["startDs"], str)
+    assert isinstance(timeline["endDs"], str)
+    assert isinstance(timeline["addTerminalStateRow"], bool)
+    assert isinstance(timeline["actionDiscrete"], bool)
+    assert isinstance(timeline["inputTableName"], str)
+    assert isinstance(timeline["outputTableName"], str)
+    assert isinstance(timeline["evalTableName"], str)
+    assert timeline["inputTableName"] != timeline["outputTableName"]
+    assert timeline["inputTableName"] != timeline["evalTableName"]
+    assert timeline["outputTableName"] != timeline["evalTableName"]
+    assert isinstance(timeline["numOutputShards"], int)
+    assert isinstance(query["tableSample"], int)
+    assert isinstance(query["actions"], list)
+    assert all([isinstance(action, str) for action in query["actions"]])
+    assert all([isinstance(ast.literal_eval(action), int) for action in query["actions"]])
