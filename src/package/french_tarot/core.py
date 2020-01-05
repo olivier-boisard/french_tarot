6ed2424f9da5a37818778796464e59bdf5751dd9
Olivier Boisard <olivier.boisard@gmail.com>
2020-01-05 17:19:43 +0100

    Implemented merge_files.

--- src/package/french_tarot/core.py
+++ src/package/french_tarot/core.py
@@ -1,5 +1,13 @@
 import time
+from typing import List
 
 
 def create_timestamp():
     return int(time.time() * 1000000)
+
+
+def merge_files(input_file_paths: List[str], output_file_path: str):
+    with open(output_file_path, 'w') as output_file:
+        for input_file_path in input_file_paths:
+            with open(input_file_path) as input_file:
+                output_file.writelines(input_file.readlines())
