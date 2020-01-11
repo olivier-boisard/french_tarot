26c5595513d90a3a0a8864a5737aeac6f1e0f1dc
Olivier Boisard <olivier.boisard@gmail.com>
2020-01-11 09:19:28 +0100

    Implemented data production script.

--- src/applications/generate_card_phase_data.py
+++ src/applications/generate_card_phase_data.py
@@ -1,5 +1,18 @@
+import argparse
+
+from french_tarot.reagent.data_producer import DataProducer
+from french_tarot.reagent.play_episodes import play_episodes
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output_file_path")
+    parser.add_argument("--n-max-episodes", type=int, default=None)
+    args = parser.parse_args()
+
+    with open(args.output_file_path, "w") as f:
+        producer = DataProducer(play_episodes(), f)
+        producer.run(args.n_max_episodes)
 
 
 if __name__ == "__main__":
