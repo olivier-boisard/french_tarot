90205af44fa55dd96fb5d9251798a826ee9d5fd6
Olivier Boisard <olivier.boisard@gmail.com>
2020-01-11 09:12:32 +0100

    Fixed type hinting.

--- src/package/french_tarot/reagent/play_episodes.py
+++ src/package/french_tarot/reagent/play_episodes.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, List
 
 from french_tarot.agents.card_phase_observation_encoder import CardPhaseObservationEncoder
 from french_tarot.play import play_episode
@@ -6,7 +6,7 @@ from french_tarot.reagent.card_phase import CardPhaseStateActionEncoder
 from french_tarot.reagent.data import ReAgentDataRow
 
 
-def play_episodes() -> Generator[ReAgentDataRow, None, None]:
+def play_episodes() -> Generator[List[ReAgentDataRow], None, None]:
     encoder = CardPhaseStateActionEncoder(CardPhaseObservationEncoder())
     while True:
         yield play_episode(encoder)
