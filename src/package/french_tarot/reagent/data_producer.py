5af9382134abc203a9742b0e5c623b893d5fe1ea
Olivier Boisard <olivier.boisard@gmail.com>
2020-01-12 18:15:36 +0100

    Implemented threaded DataProducer.

--- src/package/french_tarot/reagent/data_producer.py
+++ src/package/french_tarot/reagent/data_producer.py
@@ -8,14 +8,19 @@ class DataProducer:
     def __init__(self, data_generator: Generator[List[ReAgentDataRow], None, None], writer: IO):
         self._data_generator = data_generator
         self._writer = writer
+        self._running = False
 
     def run(self, n_max_episodes: int = None):
-        running = True
         episode = 0
-        while running:
+        self._running = True
+        while self._running:
             generated_data = next(self._data_generator)
             generated_data = [json.dumps(row.dictionary) + "\n" for row in generated_data]
             self._writer.writelines(generated_data)
 
             episode += 1
-            running = episode < n_max_episodes
+            if n_max_episodes is not None:
+                self._running = episode < n_max_episodes
+
+    def stop(self):
+        self._running = False
--- src/tests/unit/reagent/test_data_producer.py
+++ src/tests/unit/reagent/test_data_producer.py
@@ -1,17 +1,37 @@
+from threading import Thread
 from typing import List
+
+import pytest
 
 from french_tarot.reagent.data import ReAgentDataRow
 from french_tarot.reagent.data_producer import DataProducer
 
 
-def test_data_producer(mocker):
+def test_data_producer_max_episodes(mocker):
     mocked_data_row = mocker.Mock()
     mocked_data_row.dictionary = {"a": "b"}
     data_generator = fake_generator(mocked_data_row)
-
     data_saver = FakeDataSaver()
+
     producer = DataProducer(data_generator, data_saver)
     producer.run(n_max_episodes=1)
+    assert data_saver.rows is not None
+
+
+@pytest.mark.timeout(3)
+def test_data_producer_stop(mocker):
+    mocked_data_row = mocker.Mock()
+    mocked_data_row.dictionary = {"a": "b"}
+    data_generator = fake_generator(mocked_data_row)
+    data_saver = FakeDataSaver()
+
+    producer = DataProducer(data_generator, data_saver)
+    thread = Thread(target=producer.run)
+    thread.start()
+    while not producer._running:
+        pass
+    producer.stop()
+    thread.join()
     assert data_saver.rows is not None
 
 
