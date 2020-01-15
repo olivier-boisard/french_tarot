3af4d8896b763e198167724c48e18e5ff19e099c
Olivier Boisard <olivier.boisard@gmail.com>
2020-01-15 17:53:35 +0100

    Save data in temporary folder.

--- src/scripts/training.sh
+++ src/scripts/training.sh
@@ -3,6 +3,9 @@
 repo_dir=$(git rev-parse --show-toplevel)
 python_exec=$repo_dir/venv/bin/python
 python_workdir=$repo_dir/src/package
+training_folder_tmp=$repo_dir/tmp
 
 generation_module=french_tarot.applications.generate_card_phase_data
-(cd "$python_workdir" && $python_exec -m $generation_module samples.json --n-max-episodes=10000)
+mkdir -p "$training_folder_tmp"
+(cd "$python_workdir" && $python_exec -m $generation_module "$training_folder_tmp/samples.json" --n-max-episodes=10000)
+rm -rf "$training_folder_tmp"
